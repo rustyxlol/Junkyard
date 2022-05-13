bb109123ba7e9301ac8c9d3250a4d6c0ac067a3f
Rusty <imkhazix@gmail.com>
2022-05-13 22:35:09 +0530

    is this answer even correct?

--- dsapy/chapter_01/p0131.py
+++ dsapy/chapter_01/p0131.py
@@ -8,3 +8,23 @@ to the bills and coins can be based on the monetary system of any current
 or former government. Try to design your program so that it returns as
 few bills and coins as possible
 """
+CURRENCY = (100, 50, 20, 10, 5, 2, 1)
+
+
+def make_change(charged, given):
+    total = given - charged
+    bills = {i: 0 for i in CURRENCY}
+
+    for i in CURRENCY:
+        if total // i != 0:
+            bills[i] += (total // i)
+            total -= (i * (total//i))
+
+    for bill, cnt in bills.items():
+        if cnt == 0:
+            continue
+        print(f"{cnt} {bill}")
+
+
+if __name__ == "__main__":
+    make_change(1, 100)
