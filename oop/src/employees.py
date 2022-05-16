08281f452ef57316b6f89c4e4c2535e4a92faaaf
Rusty <imkhazix@gmail.com>
2022-05-16 18:43:16 +0530

    Better comments

--- oop/src/employees.py
+++ oop/src/employees.py
@@ -1,4 +1,12 @@
+"""
+Simple OOPS demonstration
+"""
+
+
 class Employee:
+    """
+    Employee class
+    """
 
     total_employees = 0
     bonus_salary = 10000
@@ -12,29 +20,54 @@ class Employee:
 
     @classmethod
     def from_string(cls, emp_string):
+        """
+        Alternative constructor
+
+        :param emp_string: of the form firstname-lastname-emailid
+        :return: Instance of Employee
+        """
         first, last, email = emp_string.split('-')
         return cls(first, last, email)
 
     def get_employee(self):
+        """
+        Returns information of an instance
+        """
         return f"First Name: {self.first_name}\nLast Name: {self.last_name}\nSalary: {self.salary}"
 
     def apply_bonus(self):
+        """
+        Increments salary
+        """
         self.salary += self.bonus_salary
 
     def __repr__(self):
-        return f"Employee('{self.first_name}', '{self.last_name}', '{self.email_id}', {self.salary})"
+        return f"Employee('{self.first_name}', \
+                          '{self.last_name}',\
+                          '{self.email_id}', \
+                           {self.salary})"
 
     def __str__(self):
         return f"Employee({self.first_name} {self.last_name} - {self.email_id})"
 
 
 class Developer(Employee):
+    """
+    Inheritance Demo 1
+    """
+
     def __init__(self, first, last, email, salary, prog_lang):
         super().__init__(first, last, email, salary)
         self.prog_lang = prog_lang
 
 
 class Manager(Employee):
+    """
+    Inheritance Demo 2
+
+    :param Employee: _description_
+    """
+
     def __init__(self, first, last, email, salary, developers=None):
         super().__init__(first, last, email, salary)
         if developers is None:
@@ -43,14 +76,23 @@ class Manager(Employee):
             self.developers = developers
 
     def add_developer(self, developer):
+        """
+        Adds a developer to a manager instance
+        """
         if developer not in self.developers:
             self.developers.append(developer)
 
     def remove_developer(self, developer):
+        """
+        Removes a developer from a manager instance
+        """
         if developer in self.developers:
             self.developers.remove(developer)
 
     def print_developers(self):
+        """
+        Prints all developers under a manager instance
+        """
         for index, developer in enumerate(self.developers):
             print(index + 1, developer)
 
