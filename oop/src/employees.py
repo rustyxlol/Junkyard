629e738e7bf639fffea22ba5e3685165ae663b8d
Rusty <imkhazix@gmail.com>
2022-05-16 18:34:20 +0530

    classmethods, inheritance and dunders

--- oop/src/employees.py
+++ oop/src/employees.py
@@ -3,13 +3,17 @@ class Employee:
     total_employees = 0
     bonus_salary = 10000
 
-    def __init__(self, first_name, last_name, employee_id, email_id, salary):
+    def __init__(self, first_name, last_name, email_id, salary=40000):
         self.first_name = first_name
         self.last_name = last_name
-        self.employee_id = employee_id
         self.email_id = email_id
         self.salary = salary
         Employee.total_employees += 1
+
+    @classmethod
+    def from_string(cls, emp_string):
+        first, last, email = emp_string.split('-')
+        return cls(first, last, email)
 
     def get_employee(self):
         return f"First Name: {self.first_name}\nLast Name: {self.last_name}\nSalary: {self.salary}"
@@ -18,20 +22,54 @@ class Employee:
         self.salary += self.bonus_salary
 
     def __repr__(self):
-        return f"Employee({self.employee_id}, {self.email_id}, {self.first_name}, {self.last_name})"
+        return f"Employee('{self.first_name}', '{self.last_name}', '{self.email_id}', {self.salary})"
+
+    def __str__(self):
+        return f"Employee({self.first_name} {self.last_name} - {self.email_id})"
+
+
+class Developer(Employee):
+    def __init__(self, first, last, email, salary, prog_lang):
+        super().__init__(first, last, email, salary)
+        self.prog_lang = prog_lang
+
+
+class Manager(Employee):
+    def __init__(self, first, last, email, salary, developers=None):
+        super().__init__(first, last, email, salary)
+        if developers is None:
+            self.developers = []
+        else:
+            self.developers = developers
+
+    def add_developer(self, developer):
+        if developer not in self.developers:
+            self.developers.append(developer)
+
+    def remove_developer(self, developer):
+        if developer in self.developers:
+            self.developers.remove(developer)
+
+    def print_developers(self):
+        for index, developer in enumerate(self.developers):
+            print(index + 1, developer)
 
 
 if __name__ == "__main__":
-    corey = Employee('Corey', 'Schafer', '420', 'cs@shafer.com', 40000)
-    norey = Employee('Norey', 'Schafer', '420', 'ns@shafer.com', 40000)
-    print(corey.get_employee())
-    print(norey.get_employee())
+    corey = Employee.from_string('Corey-Schafer-cs@youtube.com')
 
-    corey.apply_bonus()
-    print(corey.get_employee())
+    dev_1 = Developer('Rusty', 'Hawker', 'rhemail@pm.me', 5000000, 'Python')
+    dev_2 = Developer('Dusty', 'Cawker', 'dcemail@pm.me', 5000000, 'C#')
 
-    norey.bonus_salary = 12345
-    norey.apply_bonus()
-    print(norey.get_employee())
+    mgr_1 = Manager('Karen', 'Smith', 'kall@manager.com', 5000000)
 
-    print("Total employees: ", Employee.total_employees)
+    print(f"corey = {corey}")
+    print(f"dev_1 = {dev_1}")
+    print(f"dev_2 = {dev_2}")
+    print(f"mgr_1 = {mgr_1}")
+
+    mgr_1.add_developer(dev_1)
+
+    print(f"Total employees = {Employee.total_employees}")
+
+    mgr_1.print_developers()
