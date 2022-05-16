b9d9e78f2f79f04a6fac9c1be5ccb45b901cdaca
Rusty <imkhazix@gmail.com>
2022-05-16 13:28:13 +0530

    added Makefile and setup.py back

---
+++ unit-testing/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+import io
+
+from glob import glob
+from os.path import basename
+from os.path import dirname
+from os.path import join
+from os.path import splitext
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read(*names, **kwargs):
+    with io.open(
+        join(dirname(__file__), *names),
+        encoding=kwargs.get('encoding', 'utf8')
+    ) as fh:
+        return fh.read()
+
+
+setup(
+    name='Unit-testing',
+    version='1.0',
+    license='MIT License',
+    description='Unit testing and testing setup.py and Makefiles',
+    author='Rusty',
+    author_email='rustyxlol@pm.me',
+    url='https://github.com/rustyxlol/Junkyard',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    zip_safe=False,
+)
